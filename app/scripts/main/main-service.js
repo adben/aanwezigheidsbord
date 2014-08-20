60c16c51443a8fb48219e62805250aad8d50661d
Frederik Muller <fmuller@iprofs.nl>
2014-08-20 20:24:37 +0200

    Altered the service

--- app/scripts/main/main-service.js
+++ app/scripts/main/main-service.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('aanwezigheidsbord')
-        .service('MainService', function (Restangular) {
+        .service('MainService', ['Restangular', function (Restangular) {
             var baseUrl = "http://www.iprofs.nl";
             var aanwezigen = Restangular.all(baseUrl + '/user');
 
             function deleteAanwezige(name) {
                 // Route: /user/<name> DELETE: Returns Status 200/OK + Optionally a message when the user was already gone
-                currentAanwezige = aanwezigen[name];
+                var currentAanwezige = aanwezigen[name];
+                currentAanwezig.remove().then(function(response) {
+                    console.log("Object removed OK");
+                    return response;
+                }, function() {
+                    console.log("There was an error deleting");
+                });
             }
 
             function createAanwezige(aanwezige) {
@@ -15,7 +21,12 @@ angular.module('aanwezigheidsbord')
                 var newAanwezige = ['Daan'];
 
                 // POST /accounts
-                aanwezigen.post(newAanwezige);
+                aanwezigen.post(newAanwezige).then(function(response) {
+                    console.log("Object createrd OK");
+                    return response;
+                }, function() {
+                    console.log("There was an error creating");
+                });
             }
 
             function getAanwezigen(){
@@ -28,7 +39,7 @@ angular.module('aanwezigheidsbord')
                 getAanwezigen: getAanwezigen
             };
 
-        });
+        }]);
             /*
             Route: /user POST: With body {"name:"..."}: Returns Status 201/CREATED + Optionally a message when the user is already there
 
